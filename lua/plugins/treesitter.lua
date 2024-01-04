aa79beaea38b9c6d6bb33bc23d134366598d5077
Maciej Niepiekło <lewiatan.hpc@gmail.com>
2024-01-04 15:34:51 +0100

    update lua tables structure for some plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,16 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-        ensure_installed = {"lua", "vim", "vimdoc", "css", "diff", "dockerfile", "gitignore", "graphql", "html", "javascript", "jsdoc", "json", "php", "phpdoc", "regex", "typescript", "yaml", "xml"},
-        sync_instsall = false,
-        auto_install = true,
-        additional_vim_regex_highlighting = false,
-      })
-    end
-  }
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {"lua", "vim", "vimdoc", "css", "diff", "dockerfile", "gitignore", "graphql", "html", "javascript", "jsdoc", "json", "php", "phpdoc", "regex", "typescript", "yaml", "xml"},
+      sync_instsall = false,
+      auto_install = true,
+      additional_vim_regex_highlighting = false,
+    })
+  end
 }
