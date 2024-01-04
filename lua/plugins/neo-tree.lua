e662188d83eec982e8985cbfa33a0d726ff09ccd
Maciej Niepiekło <lewiatan.hpc@gmail.com>
2024-01-04 16:20:55 +0100

    close neo tree when opening file

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,18 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function(file_path)
+           require("neo-tree.command").execute({ action = "close" })
+          end
+        },
+      }
+    })
+    
 		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {})
 		vim.keymap.set("n", "<leader>fb", ":Neotree buffers reveal float<CR>", {})
 	end,
